157f87ae4372ade5a44c439ee603f4cd41d4ede9
cputnam-a11y <chrlsvryptnm@gmail.com>
2024-07-01 10:33:20 -0500

    Initial Commit

---
+++ src/main/java/bumpthis/BumpThisDataGenerator.java
@@ -0,0 +1,11 @@
+package bumpthis;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BumpThisDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
